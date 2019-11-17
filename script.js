f67c89e99ba1c501190d11582d582a83a1295908
Ash Robinson <a_p_robinson@icloud.com>
2019-11-17 23:02:22 +0000

    Added shrink effect to header on scroll.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const nav = document.getElementById('navbar');
 const mobileMenu = document.getElementById('mobileMenu');
+const header = document.getElementById('header');
 
 function showMenu() {
     nav.classList.toggle('active');
@@ -8,3 +9,17 @@ function showMenu() {
 mobileMenu.onclick = function () {
     showMenu();
 };
+
+window.onscroll = function(){
+  headerScroll()
+};
+
+function headerScroll(){
+  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60){
+    header.style.height = "60px";
+    nav.style.top = "60px";
+  } else {
+    header.style.height = "120px";
+    nav.style.top = "120px";
+  }
+}
