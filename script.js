8b1247bc038f95bd727ced8635f9447dcb5b5377
Ash Robinson <a_p_robinson@icloud.com>
2019-11-26 22:22:37 +0000

    Added music, contact and fixed mobile menu sizing issue on scroll

--- script.js
+++ script.js
@@ -18,8 +18,14 @@ function headerScroll(){
   if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60){
     header.style.height = "60px";
     nav.style.top = "60px";
+    mobileMenu.style.height = "20px";
+    mobileMenu.style.width = "20px";
+    mobileMenu.style.top = "15px";
   } else {
     header.style.height = "120px";
     nav.style.top = "120px";
+    mobileMenu.style.height = "30px";
+    mobileMenu.style.width = "30px";
+    mobileMenu.style.top = "37px";
   }
 }
